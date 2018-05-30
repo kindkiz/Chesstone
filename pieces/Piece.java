226ae5db5193426de38a36bf3846c0633d8fa952
redjen8 <seokwoo2525@gmail.com>
2018-05-30 09:28:05 +0900

    initial changes modified

--- pieces/Piece.java
+++ pieces/Piece.java
@@ -1,5 +1,9 @@
 package pieces;
 
-public abstract class Piece {
-	
+public abstract class Piece extends Position {
+	private int[] howMove = new int[2];
+	public int color;
+	public boolean isAlive;
+	public int whoRU;
+
 }
---
+++ pieces/Position.java
@@ -0,0 +1,14 @@
+package pieces;
+
+public class Position {
+	private int x;
+	private int y;
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
