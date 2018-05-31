266480bac0b7e28acb2fd3491016048133868a65
kindkiz <kindkiz@naver.com>
2018-05-31 21:51:15 +0900

    Add board

--- chess/Checker.java
+++ chess/Checker.java
@@ -2,4 +2,33 @@ package chess;
 
 public class Checker {
 	
+	public boolean isChecked(Board_1 board) {
+		
+		return false;
+	}
+	
+	public boolean isChecked(Board_2 board) {
+		
+		return false;
+	}
+	
+	public boolean isCheckMate(Board_1 board) {
+		
+		return false;
+	}
+	
+	public boolean isCheckMate(Board_2 board) {
+	
+		return false;
+	}
+	
+	public boolean isStaleMate(Board_1 board) {
+		
+		return false;
+	}
+	
+	public boolean isStaleMate(Board_2 board) {
+		
+		return false;
+	}
 }
