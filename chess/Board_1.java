8fa8d3a3c3a56d2f8fc0f17a8426e9fd37424483
redjen8 <seokwoo2525@gmail.com>
2018-05-31 22:47:16 +0900

    Board object changed to public

--- chess/Board_1.java
+++ chess/Board_1.java
@@ -1,5 +1,24 @@
 package chess;
 
+import pieces.Piece;
+import pieces.Position;
+
 public class Board_1 {
 
+	// 20180531 RedJen Initialized
+
+	public Piece[][] board = new Piece[8][8];
+	public int turn;
+	public boolean p1_catchable[][] = new boolean[8][8];
+	public boolean p2_catchable[][] = new boolean[8][8];
+
+	void Move(Position PiecePosition, Position dest) {
+		Position tempPosition = null;
+		
+	}
+
+	Piece getPiece(int PieceX, int PieceY) {
+		Piece resultPiece = null; //Board haven't enough info yet
+		return resultPiece;
+	}
 }
